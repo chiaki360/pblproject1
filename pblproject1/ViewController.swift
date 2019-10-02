635f5272e20300c03a8c51337e0deba6a5a2ea07
Chiaki <is0529vp@ed.ritsumei.ac.jp>
2019-10-02 13:34:58 +0900

    Initial Commit

---
+++ pblproject1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pblproject1
+//
+//  Created by Chiaki Yuge on 2019/10/02.
+//  Copyright © 2019 Chiaki Yuge. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ pblproject1Tests/pblproject1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  pblproject1Tests.swift
+//  pblproject1Tests
+//
+//  Created by Chiaki Yuge on 2019/10/02.
+//  Copyright © 2019 Chiaki Yuge. All rights reserved.
+//
+
+import XCTest
+@testable import pblproject1
+
+class pblproject1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
