3a3687ddf647d065c77ae885def555737e9c26e1
Ye Wente <is0537sk@ed.ritsumei.ac.jp>
2019-10-04 13:55:41 +0900

    UI image

--- pblproject1/ViewController.swift
+++ pblproject1/ViewController.swift
@@ -10,8 +10,8 @@ import UIKit
 
 class ViewController: UITableViewController {
     var pictures = [String]()
-
-
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -26,8 +26,19 @@ class ViewController: UITableViewController {
             }
         }
         print(pictures)
+        
     }
-
-
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
+    
 }
+
+
+
 
