21752efa8dc10549bf35249617bc9bdb291baa0a
Shaiful Nizam <is0535fs@ed.ritsumei.ac.jp>
2019-10-04 13:08:40 +0900

    Zam - upload the other 7 pictures

--- pblproject1/ViewController.swift
+++ pblproject1/ViewController.swift
@@ -8,9 +8,8 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
     var pictures = [String]()
-
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,6 +27,15 @@ class ViewController: UIViewController {
         print(pictures)
     }
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
 
 }
 
