d936f0d78d789104dc059a762ee4a8be0b57252a
Ye Wente <is0537sk@ed.ritsumei.ac.jp>
2019-10-04 14:09:31 +0900

    UI image

--- pblproject1/DetailViewController.swift
+++ pblproject1/DetailViewController.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 class DetailViewController: UIViewController {
-
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
